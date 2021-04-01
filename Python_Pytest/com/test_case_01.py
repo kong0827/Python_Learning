0e80ba573388759bf848ba5ecd074f2e05a9405d
kongxiangjin <1351882069@qq.com>
2021-04-01 17:16:13 +0800

    allure

---
+++ Python_Pytest/com/test_case_01.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/17 上午10:10
+# @Author  : WangJuan
+# @File    : test_case.py
+import allure
+import pytest
+
+
+@allure.feature('test_module_01')
+def test_case_01():
+    """
+    用例描述：Test case 01
+    """
+    assert 0 == 0
+
+
+@allure.feature('test_module_02')
+def test_case_02():
+    """
+    用例描述：Test case 02
+    """
+    assert 0 == 0
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', '-q', '--alluredir', './report/xml'])
